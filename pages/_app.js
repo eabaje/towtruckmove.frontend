29e8a55014109a206438e0b5915d2a81ca055aa1
Ebi Abaje <eabaje@outlook.com>
2022-11-16 23:17:32 +0100

    commit@11-116-2022

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+//import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import "../styles/globals.css";
+import GlobalProvider from "../context/Provider";
 
-export default MyApp
+import { wrapper } from "../redux/store";
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <GlobalProvider>
+      <Component {...pageProps} />
+    </GlobalProvider>
+  );
+};
+//export default App;
+export default wrapper.withRedux(App);
